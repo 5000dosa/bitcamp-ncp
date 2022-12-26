f846e3dd34d057836e906d04c6cff01b2a23f87d
5000dosa <ekdpfp@naver.com>
2022-12-26 19:48:29 +0900

    '.'

---
+++ boot-project/app/src/main/resources/templates/README.md
@@ -0,0 +1,3 @@
+# src/main/resources/templatens
+
+Thymeleaf 등에서 사용하는 탬플릿 파일을 두는 폴더
--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
 console.log("Hello,world!");
+
+let a = 300;
+
+if (a > 200 ) {
+  gkgkgk.gghg.dkdkdk("text..ok")
+}
