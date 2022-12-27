fc4eb083c0a1e7d88159cbebd1d0178c11392740
5000dosa <ekdpfp@naver.com>
2022-12-27 20:15:30 +0900

    '.'

---
+++ java/abc.java
@@ -0,0 +1,15 @@
+public class abc {
+  public static void main(String[] args) {
+    int num = 65;
+    float some = 100.14f;
+    char prefix = 'A';
+
+    System.out.println((float) num);
+    System.out.println((char) num);
+    System.out.println((int)some);
+    System.out.println((char) some);
+    System.out.println((int) prefix);
+    System.out.println((float) prefix);
+
+  }
+}
