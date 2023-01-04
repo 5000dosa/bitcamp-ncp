dc8be1afa2f454e00f921a02530398cec57bcef1
5000dosa <ekdpfp@naver.com>
2023-01-04 18:06:44 +0900

    '.'

--- eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex01/star.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex01/star.java
@@ -1,32 +1,16 @@
 package com.eomcs.lang.ex01;
 
-import java.util.Scanner;
 
-class star {
+
+public class star {
   public static void main(String[] args) {
-    Scanner KeyScan = new Scanner(System.in);
-    System.out.println("밑변의 길이?");
-    int len = KeyScan.nextInt();
-    KeyScan.close();
-
-    int starLen = 1;
-    while (starLen <= len) {
-      int spaceCnt = 1;
-      int spaceLen = (len - starLen) / 2;
-      while (spaceCnt <= spaceLen) {
-        System.out.print(" ");
-        spaceCnt++;
-      }
-
-      int starCnt = 1;
-      while (starCnt <= starLen) {
-        System.out.print("*");
-        starCnt++;
-      }
-
-      System.out.println();
-      starLen += 2;
-    }
-
+    int[] odds = new int[5];
+    odds[0] = 1;
+    odds[1] = 3;
+    odds[2] = 5;
+    odds[3] = 7;
+    odds[4] = 9;
+    odds[5] = 11;
   }
+}
 }
