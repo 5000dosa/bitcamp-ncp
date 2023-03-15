7b7fa2f382427ab551f333c2a7bcf5c2dbf8edda
5000dosa <ekdpfp@naver.com>
2023-03-15 17:23:16 +0900

    'react'

--- myapp/app-69-server/src/main/java/bitcamp/myapp/vo/Board.java
+++ myapp/app-69-server/src/main/java/bitcamp/myapp/vo/Board.java
@@ -1,21 +1,26 @@
 package bitcamp.myapp.vo;
 
 import java.sql.Date;
+import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import lombok.Data;
 
 @Data
-public class Member {
+public class Board {
   private int no;
-  private String name;
-  private String email;
+  private String title;
+  private String content;
   private String password;
-  private String tel;
 
-  //Jackson 라이브러리가 Date 타입 값을 JSON 문자열로 변환할 때 사용할 규칙을 설정한다.
   @JsonFormat(
       shape = Shape.STRING,
       pattern = "yyyy-MM-dd")
   private Date createdDate;
+
+  private int viewCount;
+  private int writerNo;
+  private String writerName;
+  private Member writer;
+  private List<BoardFile> attachedFiles;
 }
